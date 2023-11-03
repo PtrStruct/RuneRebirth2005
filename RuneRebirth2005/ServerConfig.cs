02818bb619bd0c7f62c371de85ab6f87b7638ee5
PtrStruct <va.riley@outlook.com>
2023-11-03 06:41:53 +0100

    Add server configuration and main engine for "RuneRebirth2005"
    Two main classes, ServerConfig and ServerEngine, have been added to the RuneRebirth2005 project. ServerConfig contains the basic parameters necessary for server operation, such as TICK_RATE, MAX_PLAYERS, BUFFER_SIZE, etc. The ServerEngine class is responsible for the main loop of the server.
    
    Also, the Program.cs now includes the initialization of a Serilog instance for logging purposes, and instantiates the ServerEngine to start the game server. The application now logs any encountered exceptions and can manage its runtime fluently.
    
    These changes lay the groundwork for increasing the complexity and functionality of the game server.

---
+++ RuneRebirth2005/ServerConfig.cs
@@ -0,0 +1,13 @@
+namespace RuneRebirth2005;
+
+public class ServerConfig
+{
+    public const int TICK_RATE = 600;
+    public const int MAX_PLAYERS = 2048;
+    public const int BUFFER_SIZE = 4096;
+    public const int PORT = 43594;
+    public const string SERVER_NAME = "RuneRebirth2005";
+    public static readonly string WELCOME_MSG = $"Welcome to {SERVER_NAME}";
+    public const int MAX_NPCS = 60000;
+    public const int SERVER_EXP_BONUS = 500;
+}
---
+++ RuneRebirth2005/ServerEngine.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace RuneRebirth2005;
+
+public class ServerEngine
+{
+    private bool _isRunning;
+    
+    public void Run()
+    {
+        _isRunning = true;
+        
+        while (_isRunning)
+        {
+            var stopwatch = StartStopwatch();
+            Tick();
+            SleepIfRequired(stopwatch);
+        }
+    }
+    
+    private Stopwatch StartStopwatch()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        return stopwatch;
+    }
+    
+    private void SleepIfRequired(Stopwatch stopwatch)
+    {
+        stopwatch.Stop();
+        var sleepTime = CalculateSleepTime(stopwatch.Elapsed.TotalMilliseconds);
+
+        if (sleepTime > TimeSpan.Zero)
+        {
+            Thread.Sleep(sleepTime);
+        }
+        else
+        {
+            WarnAboutDeficit(sleepTime, stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+
+    private TimeSpan CalculateSleepTime(double elapsedMilliseconds)
+    {
+        return TimeSpan.FromMilliseconds(ServerConfig.TICK_RATE - elapsedMilliseconds);
+    }
+
+    private void WarnAboutDeficit(TimeSpan sleepTime, double elapsedMilliseconds)
+    {
+        Console.WriteLine($"Server can't keep up!\nElapsed: {elapsedMilliseconds} ms\nDeficit: {-sleepTime.TotalMilliseconds} ms");
+    }
+    
+    private void Tick()
+    {
+        
+    }
+}
